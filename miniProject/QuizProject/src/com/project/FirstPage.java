103c127780aee166b7ef3a8d35ce91dd59f69c3c
snehalmagdum30 <snehalmagdum30@gmail.com>
2022-10-18 13:18:03 +0530

    helloooo

---
+++ miniProject/QuizProject/src/com/project/FirstPage.java
@@ -0,0 +1,64 @@
+package com.project;
+import java.util.Scanner;
+
+public class FirstPage {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	public void firstPage() {
+		while(true) {
+		System.out.println("      \n\n**************************WELCOME TO EXAM PORTAL**************************\n\n");
+		
+		
+		System.out.println("Please Select below option : \n\n1.Student \n2.Admin\n3.Exit");
+		
+		
+		
+		switch(scan.next().toLowerCase()) {
+		
+		case "1":
+			
+			Student1 std = new Student1();
+			std.student();
+			
+			
+			break;
+		case "2":
+			AdminLogin admin=new AdminLogin();
+		
+			admin.admin();
+			//Admin Section
+			break;
+		case "3":
+			System.out.println("\n\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+			
+			System.err.println("\n*******************************Thank you for visiting us*******************************\nyou have exited Successfully");
+			
+			System.exit(0);
+			
+		default:
+			
+			System.err.println("Wrong Input ): ");
+			System.out.println("\n\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+		
+		
+		}
+		
+		
+	}
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FirstPage fr = new FirstPage();
+		
+		fr.firstPage();
+		
+
+	}
+
+}
---
+++ miniProject/QuizProject/src/com/project/Test.java
@@ -0,0 +1,36 @@
+package com.project;
+
+
+import java.sql.SQLException;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Student std = new Student();
+//		int id = std.registerStudent();
+//		
+//		System.out.println(id);
+//		
+//		Login lo = new Login();
+//		
+//		try {
+//			lo.getUserLogin();
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		System.out.println("Hello ");
+		System.out.println("\u000C");
+		System.out.println("Hello ");
+		
+		System.out.println("Hello ");
+		
+		
+		
+	
+	}
+
+}
